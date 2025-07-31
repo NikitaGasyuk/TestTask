7e8dd4646748c3d8e9169b55b8134a110abae4a7
Nikita <nikita.gasyuk@mail.ru>
2025-07-31 11:47:47 +0300

    Подписание кода у класса Node

--- Node.cpp
+++ Node.cpp
@@ -1,45 +1,63 @@
+//подключение библиотек iostream, utility
+// подключение заголовочных файло Node, SumDispathcer, Random, Network
 #include <iostream>
 #include <utility>
 #include "Node.h"
 #include "SumDispatcher.h"
 #include "Random.h"
 #include "Network.h"
-
+// функция подписки на узел 
 void NodeSys::Node::subscribe(Node * neighbor, shared_ptr<EventDispatcher> handler)
 {
+	//провкерка условия, что подписчик(соседний узел) это не он сам
+	// и что его еще нет в списке подпсчиков 
 	if (neighbor != this && !subscribers.count(neighbor))
 	{
+		//добавляем подписку и перемещаем обработчика
 		subscribers[neighbor] = move(handler);
 	}
 }
-
+//функция отписки от узла
 void NodeSys::Node::unsubscribe(Node * neighbor)
 {
+	//инизиализируем локальную переменную присваивая найденное значение подписчика в таблице
 	auto it = subscribers.find(neighbor);
+	//проверяем найден ли подписчик
 	if (it != subscribers.end())
 	{
+		//удаляем подписчика из списка
 		subscribers.erase(it);
 	}
 }
-
+//функция уведомления подписчиков о событии
 void NodeSys::Node::notify(int data)
 {
+	//запускаем цикл, в котором происходит перебор всех подписчиков
 	for (auto& subscriber : subscribers)
 	{
+		// получаем указатель на соседний узел
 		Node* neighbor = subscriber.first;
+		// получаем обработчик событий для этого узла
 		shared_ptr<EventDispatcher> dispathcer = subscriber.second;
+		//Вызываем обработичк передавая следующие значения: данные событи,
+		// имя текущего узла и имя узла-подписчика
 		dispathcer->Handle(data, name, neighbor->getName());
 	}
 }
-
+// функция создания и подписки новых узлов
 void NodeSys::Node::createAndSubscribeNewNode()
 {
+	//генерация нового имени для узла
 	string newNodeName = Network::generateUniqueNodeName();
+	// создание нового узла, с помощью класса Random, с сгенерированным именем и передавая
+	// ссылку на буфер новых узлов
 	auto newNode = make_shared<Random>(newNodeName, newNodesBuffer);
-
+	//добавление нового узла в буфер сети
 	newNodesBuffer.push_back(newNode);
-
+	//подписка текущего узла на новый с передачей указателем на новый узешл
+	// и созданием обработчика события классом SumDispatcher
 	this->subscribe(newNode.get(), make_shared<SumDispatcher>());
+	// вывод в консоль информации о том, что узел создал новый узел и подписался на него
 	cout << name << " created new node and subscribe: " << newNodeName << endl;
 
 }
--- Node.h
+++ Node.h
@@ -1,42 +1,50 @@
+//метод препроцессора, который предотвращает многократное включение заголовочного файла,
+// который был включен ранее.
 #ifndef NODESYS_NODE_H
 #define NODESYS_NODE_H
-
+//подключение контейнера, который хранит пары "ключ-значение", использует таблицы хэширования 
+//благодаря чему обеспечивается высокая скорость доступа данных
 #include <unordered_map>
+//подключение заголовочного файла, диспетчера событий
 #include "EventDispatcher.h"
-
+// использование стандартного пространства имён
 using namespace std;
 
+// подключение пространства имён
 namespace NodeSys
-{
+{ 
+	//объявление класса
 	class Node
 	{
+
 	protected:	
+		// имя узла
 		string name;
-		
+		// контейнер подписчиков(соседей) и их обработчик событий
 		unordered_map<Node *, shared_ptr<EventDispatcher>> subscribers;
-		
+		// ссылка на буфер новых узлов в сети (для создания новых узлов)
 		vector<shared_ptr<Node>> &newNodesBuffer;
 	
 	public:
-
+		// конструктор, который принимает имя узлла, ссылку на буфер новых узлов
 		Node(string NodeName, vector<shared_ptr<Node>> &buffer) : name(move(NodeName)), newNodesBuffer(buffer) {}
-
+		// виртуальная функция, которая обновляет статус узла
 		virtual void update() = 0;
-
+		// виртуальная функция, которая создает событие узла 
 		virtual int generateEvent() = 0;
-
+		// функция подписки на узел 
 		void subscribe(Node * neighbor, shared_ptr<EventDispatcher> handler);
-
+		//функция отписки от узла
 		void unsubscribe(Node * neighbor);
-
+		// функция уведомления подписчиков о событии 
 		void notify(int data);
-
+		//геттер функция получения имени узла
 		const string& getName() const { return name; }
-
+		//геттер функция проверки наличия подписчика
 		bool hasNoSubsribe() const { return subscribers.empty(); }
-
+		//функция создания и подписки на новый узел 
 		void createAndSubscribeNewNode();
-
+		// виртуальный деструктор
 		virtual ~Node() = default;
 	};
 };
