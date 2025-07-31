42fbd8401817b48a0c599248a53441aa73a2fda7
Nikita <nikita.gasyuk@mail.ru>
2025-07-31 13:29:04 +0300

    пояснение кода в классе SumDispatcher

--- SumDispatcher.cpp
+++ SumDispatcher.cpp
@@ -1,8 +1,10 @@
-#include <iostream>
+// подключение заголовочного файла
 #include "SumDispatcher.h"
-
+// функци€ обработчика событий
 void NodeSys::SumDispatcher::Handle(int data, const string &sender, const string &receiver)
 {
+	// увеличение суммы узлов сети добавл€€ к нему полученные данные
 	sum += data;
+	// вывод в консоль им€ узла отправител€ -> им€ узла получател€ и текущую сумму узлов сети
 	cout << sender << " -> " << receiver << ": S = " << sum << endl;
 }
--- SumDispatcher.h
+++ SumDispatcher.h
@@ -1,19 +1,25 @@
+//метод препроцессора, который предотвращает многократное включение заголовочного файла,
+// который был включен ранее.
 #ifndef NODESYS_SUMDISPATCHER_H
 #define NODESYS_SUMDISPATCHER_H
 
+//подключение библиотеки iostream
+#include <iostream>
+// подключение заголовочного файла EventDispatcher
 #include "EventDispatcher.h"
 
 using namespace std;
 
 namespace NodeSys
 {
+	// объ€вление класса SumDispatcher унаследованного от EventDispathcer
 	class SumDispatcher : public EventDispatcher
 	{
-
+		// инициализаци€ целочисленной переменной суммы узлов сети
 		int sum = 0;
 
 	public:
-
+		// виртуальна€ функи€ обработчика событий
 		virtual void Handle(int data, const string &sender, const string &receiver) override;
 
 	};
