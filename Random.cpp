9e4e2b00c2d26c27b1debbdc31a8b86ad847c34f
Nikita <nikita.gasyuk@mail.ru>
2025-07-31 13:06:14 +0300

    описание кода в классе Random

--- Random.cpp
+++ Random.cpp
@@ -1,23 +1,30 @@
-#include <random>
+//подключается заголовочный файл
 #include "Random.h"
-
+//функция обновления состояния узла
 void NodeSys::Random::update()
 {
+	//проверка наличия подписчика
 	if (hasNoSubsribe())
 	{
+		// возврат
 		return;
 	}
+	// инициализация целочисленой переменной которая принимает значения сгенерированного события
 	int eventData = generateEvent();
+	//вызов функции уведомления  и рассылка сгенерированного события подписчикам
 	notify(eventData);
-
+	// создание распределения 0-5
 	uniform_int_distribution<> chance(0, 4);
+	//проверка на равенство нулю
 	if (chance(gen) == 0)
 	{
+		// вызов функции 
 		createAndSubscribeNewNode();
 	}
 }
-
+// функция создания события 
 int NodeSys::Random::generateEvent()
 {
+	// возвращение случайного числа от 0-100
 	return dis(gen);
 }
--- Random.h
+++ Random.h
@@ -1,27 +1,35 @@
+//метод препроцессора, который предотвращает многократное включение заголовочного файла,
+// который был включен ранее.
 #ifndef NODESYS_RANDOM_H
 #define NODESYS_RANDOM_H
-
+//подключение библиотеки рандом
 #include <random>
+//подключение заголовочного файла Node
 #include "Node.h"
 
 using namespace std;
 
 namespace NodeSys
 {
+	//объ€вление класса Random с унаследованием от класса Node
 	class Random : public Node
 	{
+		// устройство дл€ получени€ случайных чисел
 		random_device rd;
-		
+		// генератор случайных чисел
 		mt19937 gen;
-		
+		//устройство дл€ равномерного распределени€ целых чисел
 		uniform_int_distribution<> dis;
 
 	public:
-
+		// конструктор, который получает им€ узла и буфер новых узлов,
+		// вызываетс€ конструктор базового класса
+		// инициализируетс€ генератор случайных чисел
+		// инициализируетс€ устройство распределени€ целых чисел от 1-100
 		Random(const string &name, vector<shared_ptr<Node>> &buffer) : Node(name, buffer), gen(rd()), dis(1, 100) {}
-
+		// виртуальна€ функци€ создани€ событи€
 		virtual int generateEvent() override;
-
+		// виртуальна€ функци€ обновлени€ состо€ни€ узла
 		virtual void update() override;
 
 	};
